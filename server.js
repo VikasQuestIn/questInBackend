7281cab3a5c518a5a0e160fa61269ab6372c3b01
Rohit Bisht <rohitbisht@Rohits-MacBook-Air.local>
2023-04-05 23:40:32 +0530

    changes:

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-// console.log("Node  js ")
 const express=require('express');
-const router=require('express').Router();
 const app =express();
 const PORT=process.env.PORT || 3000
-
 // database connection 
-
 const connectDB=require('./config/db')
 connectDB();
-
-
 //  All Routes 
 const userRouter=require('./routes/userRouter');
-const adminRouter=require('./routes/adminRouter');
-const flowController=require('./routes/flowRouter');
-
 app.use('/api/user',userRouter);
-app.use('/api/admin',adminRouter);
-app.use('/api/flows',flowController)
-
 app.get('/',(req,res)=>{
     res.json({msg:'Welcome to BTC QuestionIn'})
 })
-
 app.listen(PORT,()=>{
     console.log(`Listing on port ${PORT}`)
 })
