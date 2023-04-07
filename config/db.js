5159a1b64ef2e3595ed81f87d402ea6417510c49
Rohit Bisht <rohitbisht@Rohits-MacBook-Air.local>
2023-04-07 17:14:16 +0530

    7april

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 function connectDB() {
     // Database connection 
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect("mongodb+srv://questionin:questionin@atlascluster.rhb4ikg.mongodb.net/questionIn?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
     const connection = mongoose.connection;
     connection.once('open', () => {
         console.log('Database connected ');
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express=require('express');
 const app =express();
 const PORT=process.env.PORT || 8001
 // database connection 
-// const connectDB=require('./config/db')
-// connectDB();
+const connectDB=require('./config/db')
+connectDB();
 //  All Routes 
 const userRouter=require('./routes/userRouter');
 app.use('/api/user',userRouter);
 app.get('/',(req,res)=>{
     console.log("Vikas route")
-    res.json({msg:'Welcome to BTC QuestionIn made by vikas tiwari'})
+    res.json({msg:'Welcome to QuestionIn made by vikas tiwari'})
 })
 app.listen(PORT,()=>{
     console.log(`Listing on port ${PORT}`)
